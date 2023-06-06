a68a3ea161f32b7bfe54034a946054d16b172e3b
Sky Kosiner <sky@skykosiner.co.uk>
2023-06-06 21:37:13 +0100

    no target?

--- dayone/target/debug/deps/aoc_rust-18ee626334bb1d45.d
+++ dayone/target/debug/deps/aoc_rust-18ee626334bb1d45.d
@@ -1,5 +1,5 @@
-/home/sky/personal/aoc-rust/target/debug/deps/aoc_rust-18ee626334bb1d45.rmeta: src/main.rs
+/home/sky/personal/aoc-rust/dayone/target/debug/deps/aoc_rust-18ee626334bb1d45.rmeta: src/main.rs
 
-/home/sky/personal/aoc-rust/target/debug/deps/aoc_rust-18ee626334bb1d45.d: src/main.rs
+/home/sky/personal/aoc-rust/dayone/target/debug/deps/aoc_rust-18ee626334bb1d45.d: src/main.rs
 
 src/main.rs:
--- dayone/target/debug/deps/aoc_rust-a4e169ad7dd4916f.d
+++ dayone/target/debug/deps/aoc_rust-a4e169ad7dd4916f.d
@@ -1,5 +1,5 @@
-/home/sky/personal/aoc-rust/target/debug/deps/aoc_rust-a4e169ad7dd4916f.rmeta: src/main.rs
+/home/sky/personal/aoc-rust/dayone/target/debug/deps/aoc_rust-a4e169ad7dd4916f.rmeta: src/main.rs
 
-/home/sky/personal/aoc-rust/target/debug/deps/aoc_rust-a4e169ad7dd4916f.d: src/main.rs
+/home/sky/personal/aoc-rust/dayone/target/debug/deps/aoc_rust-a4e169ad7dd4916f.d: src/main.rs
 
 src/main.rs:
